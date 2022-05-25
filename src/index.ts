b30b29100c1488131e6f773a23d8c1cced2494d3
osyrisrblx <osyrisrblx@gmail.com>
2022-05-24 22:41:55 -0400

    prototyping

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,47 @@
-import { Request, Response } from 'express'
-import fetch from 'node-fetch'
-import org from 'libnpmorg'
-import team from 'libnpmteam'
-import { URLSearchParams } from 'url'
+import { Request, Response } from "express";
+import org from "libnpmorg";
+import team from "libnpmteam";
 
-require('dotenv').config()
+// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
+require("dotenv").config();
 
-const CAPTCHA_VERIFY_URL = 'https://www.google.com/recaptcha/api/siteverify'
-const NPM_TOKEN = { token: process.env.NPM_TOKEN! }
-const BANNED_USERNAMES = process.env.BANNED_USERNAMES!.split(',')
+const NPM_TOKEN = { token: process.env.NPM_TOKEN! };
+const BANNED_USERNAMES = process.env.BANNED_USERNAMES!.split(",");
 
-const badRequest = (response: Response, error: string) => (
-  response.status(400).send(`Bad request: ${error}`).end()
-)
+const badRequest = (response: Response, error: string) => response.status(400).send(`Bad request: ${error}`).end();
 
-async function validateCaptcha (response: string) {
-  const form = new URLSearchParams()
+async function addUserToOrg(username: string) {
+	if (BANNED_USERNAMES.includes(username)) return false;
 
-  form.append('secret', process.env.CAPTCHA_SECRET!)
-  form.append('response', response)
+	const profile = await org.set("rbxts", username, "developer", NPM_TOKEN);
 
-  const captchaResponse = await fetch(CAPTCHA_VERIFY_URL, {
-    method: 'POST',
-    body: form
-  })
+	const teamName = `@rbxts:${profile.user}`;
 
-  return (await captchaResponse.json()).success === true
+	let failed = false;
+
+	try {
+		await team.create(teamName, NPM_TOKEN);
+	} catch (e) {
+		failed = true;
+	}
+
+	try {
+		await team.add(profile.user, teamName, NPM_TOKEN);
+	} catch (e) {
+		failed = true;
+	}
+
+	return !failed;
 }
 
-async function addUserToOrg (username: string) {
-  if (BANNED_USERNAMES.includes(username)) return false
+export async function handleRequest(request: Request, response: Response) {
+	if (request.method !== "POST") return badRequest(response, "Wrong method");
 
-  const profile = await org.set('rbxts', username, 'developer', NPM_TOKEN)
+	if (!request.body.username) return badRequest(response, "Missing username");
 
-  const teamName = `@rbxts:${profile.user}`
-  await team.create(teamName, NPM_TOKEN)
-  await team.add(profile.user, teamName, NPM_TOKEN)
+	if (await addUserToOrg(request.body.username)) {
+		return response.status(301).set("location", `${process.env.SITE_URL!}/success`).end();
+	}
 
-  return true
-}
-
-export async function handleRequest (request: Request, response: Response) {
-  if (request.method !== 'POST') return badRequest(response, 'Wrong method')
-
-  if (!request.body.username) return badRequest(response, 'Missing username')
-
-  if (!await validateCaptcha(request.body['g-recaptcha-response'])) return badRequest(response, 'Bad captcha')
-
-  if (await addUserToOrg(request.body.username)) {
-    return response.status(301).set('location', `${process.env.SITE_URL!}/success`).end()
-  }
-
-  response.status(302).set('location', `${process.env.SITE_URL!}/fail`).end()
+	response.status(302).set("location", `${process.env.SITE_URL!}/fail`).end();
 }
