dda93c80ab5755cf9f7c22276947da97f6ec255b
osyrisrblx <osyrisrblx@gmail.com>
2024-08-07 15:27:48 -0700

    Rewrite everything :tada:

--- src/typings/libnpm.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "libnpmorg" {
-  function set (org: string, user: string, role: string, options: { token: string }): Promise<any>
-}
-
-declare module "libnpmteam" {
-  function create (team: string, options: { token: string }): Promise<any>
-  function add (user: string, team: string, options: { token: string }): Promise<any>
-}
