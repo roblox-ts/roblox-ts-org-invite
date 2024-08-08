abc48b95590e659c199732edf3bd0ba4051441c2
osyrisrblx <osyrisrblx@gmail.com>
2024-08-07 23:19:43 -0700

    Elysia

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import { $, type ShellError } from "bun";
 import assert from "node:assert";
-import express, { type Response } from "express";
-import bodyParser from "body-parser";
+import { Elysia, redirect, t } from "elysia";
+
+assert(Bun.env.PORT, "PORT is required");
+assert(Bun.env.NPM_TOKEN, "NPM_TOKEN is required");
 
 const CAPTCHA_VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify";
 
-async function validateCaptcha(response: string) {
+async function validateCaptcha(body: { "g-recaptcha-response": string }) {
 	assert(Bun.env.CAPTCHA_SECRET, "CAPTCHA_SECRET is required");
 
 	const parsed = await fetch(CAPTCHA_VERIFY_URL, {
 		method: "POST",
 		body: new URLSearchParams({
 			secret: Bun.env.CAPTCHA_SECRET,
-			response,
+			response: body["g-recaptcha-response"],
 		}),
 	}).then(res => res.json());
 
@@ -29,60 +31,62 @@ const ALREADY_IN_ORG =
 const ALREADY_SENT_INVITE =
 	"409 Conflict - PUT https://registry.npmjs.org/-/org/rbxts/user - User/Email already has a pending invite";
 
-const success = (res: Response) => res.status(302).set("location", SUCCESS_URL).end();
-
-const inviteSent = (res: Response, username: string) => {
-	const url = new URL(INVITE_SENT_URL);
-	url.searchParams.set("username", username);
-	res.status(302).set("location", url.toString()).end();
-};
-
-const badRequest = (res: Response, error: string) => {
-	const url = new URL(FAIL_URL);
-	url.searchParams.set("reason", error);
-	res.status(302).set("location", url.toString()).end();
-};
-
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (_, res) => res.send("ok"));
-
-app.post("/invite-to-org", async (req, res) => {
-	const username = req.body.username;
-
-	if (!username) return badRequest(res, "Missing username");
-
-	if (!(await validateCaptcha(req.body["g-recaptcha-response"]))) return badRequest(res, "Bad captcha");
-
-	try {
-		await $`npm org set rbxts ${username} developer --json`;
-		return inviteSent(res, username);
-	} catch (e) {
-		const shellError = e as ShellError;
-		const error = shellError.json().error.summary;
-		if (error === ALREADY_IN_ORG) return success(res);
-		if (error === ALREADY_SENT_INVITE) return inviteSent(res, username);
-		return badRequest(res, error);
+function urlParams(url: string, params: { [K in string]: string }) {
+	const parsed = new URL(url);
+	for (const [key, value] of Object.entries(params)) {
+		parsed.searchParams.set(key, value);
 	}
-});
+	return parsed.toString();
+}
 
-app.post("/add-to-team", async (req, res) => {
-	const username = req.body.username;
+const app = new Elysia();
 
-	if (!username) return badRequest(res, "Missing username");
+app.get("/", () => "ok");
 
-	if (!(await validateCaptcha(req.body["g-recaptcha-response"]))) return badRequest(res, "Bad captcha");
+app.post(
+	"/invite-to-org",
+	async ({ body }) => {
+		if (!(await validateCaptcha(body))) return redirect(urlParams(FAIL_URL, { reason: "Bad captcha" }));
+		const { username } = body;
+		try {
+			await $`npm org set rbxts ${username} developer --json`;
+			return redirect(urlParams(INVITE_SENT_URL, { username }));
+		} catch (e) {
+			const shellError = e as ShellError;
+			const error = shellError.json().error.summary;
+			if (error === ALREADY_IN_ORG) return redirect(SUCCESS_URL);
+			if (error === ALREADY_SENT_INVITE) return redirect(urlParams(INVITE_SENT_URL, { username }));
+			return redirect(urlParams(FAIL_URL, { reason: error }));
+		}
+	},
+	{
+		body: t.Object({
+			username: t.String(),
+			"g-recaptcha-response": t.String(),
+		}),
+	},
+);
 
-	try {
-		await $`npm team create @rbxts:${username}`.nothrow();
-		await $`npm team add @rbxts:${username} ${username} --json`;
-		return success(res);
-	} catch {
-		return inviteSent(res, username);
-	}
-});
+app.post(
+	"/add-to-team",
+	async ({ body }) => {
+		if (!(await validateCaptcha(body))) return redirect(urlParams(FAIL_URL, { reason: "Bad captcha" }));
+		const { username } = body;
+		try {
+			await $`npm team create @rbxts:${username}`.nothrow();
+			await $`npm team add @rbxts:${username} ${username} --json`;
+			return redirect(SUCCESS_URL);
+		} catch {
+			return redirect(urlParams(INVITE_SENT_URL, { username }));
+		}
+	},
+	{
+		body: t.Object({
+			username: t.String(),
+			"g-recaptcha-response": t.String(),
+		}),
+	},
+);
 
 app.listen(Bun.env.PORT);
 
